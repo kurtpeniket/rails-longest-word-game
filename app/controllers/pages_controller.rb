5d01ca78dc99df65b12412c59b2f9a9f48545b93
Kurt Peniket <kurt.peniket@gmail.com>
2021-02-15 16:22:49 +0000

    still need to output score

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
 
   def score
     @attempt = params[:word]
-    run_game(@attempt, grid, 10, 5)
+    run_game(@attempt, grid)
   end
 
   private 
@@ -22,37 +22,19 @@ class PagesController < ApplicationController
     @attempt.chars.all? { |letter| @attempt.count(letter) <= @grid.count(letter) }
   end
   
-  def compute_score(attempt, time_taken)
-    time_taken > 60.0 ? 0 : @attempt.size * (1.0 - time_taken / 60.0)
-  end
+   def run_game(attempt, grid)
 
-  def run_game(attempt, grid, start_time, end_time)
-
-    total_time = end_time - start_time
-  
     url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
     data = URI.open(url).read
     hash_check = JSON.parse(data)
     if hash_check["found"] && in_grid?(@attempt.upcase, @grid) == true
-      return @result = {
-        time: total_time,
-        score: compute_score(@attempt, total_time),
-        message: "well done!"
-      }
+      return "Well done!"
     elsif hash_check["found"] == true && in_grid?(@attempt.upcase, @grid) == false
-      return @result = {
-        time: total_time,
-        score: 0,
-        message: "not in the grid"
-      }
+      return "The letters for #{@attempt} were not in the grid"
     elsif hash_check["found"] == false && in_grid?(@attempt.upcase, @grid) == true
-      return @result = {
-        time: total_time,
-        score: 0,
-        message: "not an english word"
-      }
+      return "That was not a vaild English word"
     else
-      return "invaild word"
+      return "Invaild word"
     end
   end
 end
